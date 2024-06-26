d9cbdf41ace29453036cad736ad1ea3385fca38b
SAIF Ali <143219742+saifaliTheDev@users.noreply.github.com>
2024-06-26 23:49:49 +0500

    Add files via upload

---
+++ for loop and while loop/script.js
@@ -0,0 +1,29 @@
+for (var i = 1; i <= 5; i++) {
+  console.log(i);
+}
+console.log("counting finished!");
+
+for (var i = 5; i > 0; i--) {
+  console.log(i);
+}
+console.log("countdown finished!");
+
+var i = 1;
+while (i <= 5) {
+  console.log(i);
+  i = i + 1;
+}
+console.log("counting finished");
+
+var i = 5;
+while (i > 0) {
+  console.log(i);
+  i = i - 1;
+}
+console.log("countdown finished");
+
+var year = 2018;
+while (year <= 2022) {
+  console.log(year);
+  year = year + 1;
+}
